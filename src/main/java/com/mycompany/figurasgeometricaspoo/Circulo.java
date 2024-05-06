5f85acd4fad275800684d277c0399351dc816507
Dywrench <jv88611@gmail.com>
2024-05-05 19:53:53 -0500

    primera versión programa figuras geométricas

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,20 @@
 package com.mycompany.figurasgeometricaspoo;
 
 public class Circulo {
-    
+
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
 }
+
+
--- src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
@@ -1,5 +1,55 @@
 package com.mycompany.figurasgeometricaspoo;
+import java.util.Scanner;
 
-public class FiguraGeometrica {
-    
+abstract class FiguraGeometrica {
+    abstract double calcularArea();
 }
+
+// Clase para representar un círculo
+class Circulo extends FiguraGeometrica {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+}
+
+// Clase para representar un rectángulo
+class Rectangulo extends FiguraGeometrica {
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double lado1, double lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    @Override
+    double calcularArea() {
+        return lado1 * lado2;
+    }
+}
+
+// Clase para representar un triángulo
+class Triangulo extends FiguraGeometrica {
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    
+    double calcularArea() {
+        return 0.5 * base * altura;
+    }
+}
+
+
+
